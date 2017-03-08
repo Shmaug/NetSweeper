3c61c4495b4506d10bcf242a184a8f83351dfa72
trevo <trevo@FLIPPY-LAPTOP>
2017-03-08 13:07:31 -0800

    Add project files.

---
+++ Netsweeper/Game.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace NetSweeper {
+    struct Point {
+        public int x, y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    class Game {
+        public struct Tile {
+            public int neighborMineCount;
+            public bool isMine;
+            public bool isExposed;
+        }
+
+        public int gameSize { get; private set; }
+        public int turns { get; private set; }
+        public bool gameOver { get; private set; }
+
+        public Tile[,] tiles { get; private set; }
+        
+        public Game(int gameSize = 8, int mineCount = 10, int gameSeed = -1) {
+            this.gameSize = gameSize;
+
+            tiles = new Tile[gameSize, gameSize];
+
+            var neighbors = new Point[]{
+                new Point(-1, -1),
+                new Point( 1, -1),
+                new Point(-1,  1),
+                new Point( 1,  1),
+                new Point( 1,  0),
+                new Point(-1,  0),
+                new Point( 0,  1),
+                new Point( 0, -1),
+            };
+
+            // place mines randomly
+            Random r = gameSeed >= 0 ? new Random(gameSeed) : new Random();
+            int x = r.Next(0, gameSize), y = r.Next(0, gameSize);
+            for (int i = 0; i < mineCount; i++) {
+                while (tiles[x, y].isMine) { // make sure we pick a spot that isn't already a mine
+                    x = r.Next(0, gameSize);
+                    y = r.Next(0, gameSize);
+                }
+                tiles[x, y].isMine = true;
+                foreach (Point c in neighbors)
+                    if (x + c.x >= 0 && x + c.x < gameSize &&
+                        y + c.y >= 0 && y + c.y < gameSize)
+                        tiles[x + c.x, y + c.y].neighborMineCount++;
+            }
+
+            turns = 0;
+            gameOver = false;
+        }
+
+        public void PickTile(int x, int y) {
+            tiles[x, y].isExposed = true;
+            turns++;
+
+            if (tiles[x, y].isMine)
+                gameOver = true;
+
+            if (tiles[x, y].neighborMineCount == 0) {
+                // flood fill empty tiles
+                FloodFillEmpty(x, y);
+            }
+        }
+
+        void FloodFillEmpty(int x, int y) {
+            List<Point> leads = new List<Point>();
+            List<Point> nleads = new List<Point>();
+
+            if (FloodCheckTile(x + 1, y)) leads.Add(new Point(x + 1, y));
+            if (FloodCheckTile(x - 1, y)) leads.Add(new Point(x - 1, y));
+            if (FloodCheckTile(x, y + 1)) leads.Add(new Point(x, y + 1));
+            if (FloodCheckTile(x, y - 1)) leads.Add(new Point(x, y - 1));
+
+            while (leads.Count > 0) {
+                nleads.Clear();
+                foreach (Point p in leads) {
+                    if (FloodCheckTile(p.x + 1, p.y)) nleads.Add(new Point(p.x + 1, p.y));
+                    if (FloodCheckTile(p.x - 1, p.y)) nleads.Add(new Point(p.x - 1, p.y));
+                    if (FloodCheckTile(p.x, p.y + 1)) nleads.Add(new Point(p.x, p.y + 1));
+                    if (FloodCheckTile(p.x, p.y - 1)) nleads.Add(new Point(p.x, p.y - 1));
+                }
+                leads.Clear();
+                leads.AddRange(nleads);
+            }
+        }
+
+        bool FloodCheckTile(int x, int y) {
+            if (x >= 0 && x < gameSize && y >= 0 && y < gameSize) {
+                if (!tiles[x, y].isExposed) {
+                    tiles[x, y].isExposed = true;
+                    return tiles[x, y].neighborMineCount == 0; // only search off this tile if it is also empty
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Netsweeper/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetSweeper {
+    public partial class MainWindow : Window {
+        SolidColorBrush redBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+        SolidColorBrush blackBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        SolidColorBrush grayBrush = new SolidColorBrush(Color.FromRgb(127, 127, 127));
+        SolidColorBrush whiteBrush = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+
+        Label[,] labelGrid;
+        GameController controller;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            controller = new GameController(new Game());
+
+            gameGrid.Columns = controller.game.gameSize;
+            gameGrid.Rows = controller.game.gameSize;
+            labelGrid = new Label[controller.game.gameSize, controller.game.gameSize];
+            double bw = gameGrid.Width / controller.game.gameSize;
+            double bh = gameGrid.Height / controller.game.gameSize;
+            for (int x = 0; x < controller.game.gameSize; x++)
+                for (int y = 0; y < controller.game.gameSize; y++) {
+                    labelGrid[x, y] = new Label();
+                    labelGrid[x, y].Width = bw;
+                    labelGrid[x, y].Height = bh;
+                    labelGrid[x, y].Tag = new Point(x, y);
+                    labelGrid[x, y].MouseDown += Label_MouseDown;
+                    labelGrid[x, y].BorderBrush = blackBrush;
+                    labelGrid[x, y].BorderThickness = new Thickness(.5, .5, .5, .5);
+                    labelGrid[x, y].Background = grayBrush;
+                    
+                    gameGrid.Children.Add(labelGrid[x, y]);
+                }
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e) {
+            if (sender is Label) {
+                Label l = sender as Label;
+                if (l.Tag is Point) {
+                    controller.game.PickTile((l.Tag as Point?).Value.x, (l.Tag as Point?).Value.y);
+                    UpdateGame();
+                }
+            }
+        }
+
+        void UpdateGame() {
+            for (int x = 0; x < controller.game.gameSize; x++)
+                for (int y = 0; y < controller.game.gameSize; y++) {
+                    if (controller.game.tiles[x, y].isExposed) {
+                        labelGrid[x, y].Background = controller.game.tiles[x, y].isMine ? redBrush : whiteBrush;
+                        labelGrid[x, y].Content =
+                            controller.game.tiles[x, y].isMine ? "X" :
+                            (controller.game.tiles[x, y].neighborMineCount > 0 ? controller.game.tiles[x, y].neighborMineCount.ToString() : "");
+                    } else {
+                        labelGrid[x, y].Background = grayBrush;
+                    }
+                }
+        }
+    }
+}
